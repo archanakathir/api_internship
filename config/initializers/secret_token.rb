a2095ce92f41f0e8bfcf0430597d339e2427f5bf
archanakathir <archanakathir56@gmail.com>
2016-02-17 12:50:18 +0530

    initializes  the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApiProject::Application.config.secret_key_base = 'c5080ec57d006dc0b352de938f5afbe56f68372d00b3c6255949d65d08467078291aa490f982507b319b9f9b16503184a98b642aeaf628a2dce3622c6eb768c9'
